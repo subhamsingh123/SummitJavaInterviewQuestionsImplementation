4e0f5c35fcd860aeda3bdb721d2bdb10ed9b3fdf
Subham Singh <subhams@azuga.com>
2025-04-11 23:56:11 +0530

    Added Chapter 2

---
+++ Chapter2/Question1.java
@@ -0,0 +1,13 @@
+package Chapter2;
+
+//  Which programming paradigms does Java support?
+// 1) Object-Oriented Programming (OOP): Java emphasizes
+// classes and objects, with support for inheritance,
+// encapsulation, polymorphism, and abstraction.
+class Question1{
+
+public static void main(String[] args) {
+    System.out.println("hell");
+}
+
+}
